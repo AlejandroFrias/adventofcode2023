4e6f32e82f3366bd1a9f85b9fff18085a8cc97d8
Alejandro Frias <3598338+AlejandroFrias@users.noreply.github.com>
2023-12-03 19:31:27 -0600

    day2

--- day2/day2.ts
+++ day2/day2.ts
@@ -5,8 +5,40 @@ const main = () => {
   const rows = inputRaw.trim().split('\n')
   const games = Object.fromEntries(rows.map((row) => {
     const [gameNumber, rounds] = row.split(': ')
-    return [gameNumber, rounds.split('; ')]
+    return [gameNumber.split(' ')[1], rounds.split('; ').map((round) => {
+      const draws = round.split(', ')
+      return Object.fromEntries(draws.map((draw) => {
+        const [value, color] = draw.split(' ')
+        return [color, parseInt(value)]
+      }))
+    })]
   }))
-  console.log(games)
+  let validGameIdTotal = 0
+  for (const [gameId, rounds] of Object.entries(games)) {
+    let isValid = true
+    for (const round of rounds) {
+      if (round['red'] > 12 || round['green'] > 13 || round['blue'] > 14) {
+        isValid = false
+      }
+    }
+    if (isValid) {
+      validGameIdTotal += parseInt(gameId)
+    }
+  }
+  console.log({ validGameIdTotal })
+  let sumOfPowers = 0
+  for (const [gameId, rounds] of Object.entries(games)) {
+    let minRed = 0
+    let minGreen = 0
+    let minBlue = 0
+    for (const round of rounds) {
+      minRed = Math.max(minRed, round['red'] ?? 0)
+      minGreen = Math.max(minGreen, round['green'] ?? 0)
+      minBlue = Math.max(minBlue, round['blue'] ?? 0)
+    }
+    sumOfPowers += minRed * minGreen * minBlue
+  }
+  console.log({ sumOfPowers })
+
 }
 main()
